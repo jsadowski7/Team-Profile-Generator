96e9be336e570ac10bafffd7da2132b29d4456da
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-11-17 23:25:46 -0600

    Finished Manager test and wrote manager.js file, removed notes from other files

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,15 @@
 const Manager = require('../lib/Manager');
+
+// Creates a manager object
+test('Creates a Manager object', () => {
+    const manager = new Manager('Jake', 90, 'jasadowski7@gmail.com');
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// Get role from getRole()
+test('Gets role of Employee', () => {
+    const manager = new Manager('Jake', 90, 'jasadowski7@gmail.com');
+
+    expect(manager.getRole()).toEqual("Manager");
+});
