5b5f7c4ee7e2f064639a9aa87cacb2d0770b975d
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-11-17 23:17:35 -0600

    Finished Intern test and wrote Intern.js files

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,22 @@
 const Intern = require('../lib/Intern');
+
+// Creates an intern object
+test('Creates an Intern object', () => {
+    const intern = new Intern('Jake', 90, 'jasadowski7@gmail.com', 'UT');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+// Gets school from getSchool()
+test('Gets Employee School', () => {
+    const intern =  new Intern('Jake', 90, 'jasadowski7@gmail.com', 'UT');
+
+    expect(inter.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+// Gets role from getRole()
+test('Gets role of Employee', () => {
+    const intern =  new Intern('Jake', 90, 'jasadowski7@gmail.com', 'UT');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
