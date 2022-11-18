02919d2bd008b0f0206cdf7e96954332fe89d383
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-11-17 18:27:48 -0600

    Fixed console bug in package.json, added given information/constructors into lib/ files

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -7,3 +7,30 @@ test('Creates a new Employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+// Gets employee name from getName()
+test('Gets Employee name', () => {
+    const employee = new Employee('Jake', 90, 'jasadowski7@gmail.com');
+  
+    expect(employee.getName()).toEqual(expect.any(String));
+  });
+  
+  // Gets employee ID from getId()
+  test('Gets Employee ID', () => {
+    const employee = new Employee('Jake', 90, 'jasadowski7@gmail.com');
+  
+    expect(employee.getId()).toEqual(expect.any(Number));
+  });
+  
+  // Gets employee Email from getEmail()
+  test('Gets Employee Email', () => {
+    const employee = new Employee('Jake', 90, 'jasadowski7@gmail.com');
+  
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+  });
+  
+  test('Gets role of Employee', () => {
+    const employee = new Employee('Jake', 90, 'jasadowski7@gmail.com');
+  
+    expect(employee.getRole()).toEqual("Employee");
+  });
