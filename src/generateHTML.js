d201c142ee76ad56311903490a8cff59d0f7fd52
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2022-11-20 20:51:38 -0600

    Finished writing generateHTML file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -104,9 +104,26 @@ const generateTeamPage = function (employeeCards) {
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-            
-    </body>`
-}
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span> 
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!-- Team Cards -->
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </html>
+  `;
+};
 
 // Export to Index
 module.exports = generateHTML;
